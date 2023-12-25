6c23887163b5a84e9f5c28a77e2a7208a08667f9
chikanabila <119095959+chikanabila@users.noreply.github.com>
2023-12-25 20:36:07 +0700

    Update Tabel.js

--- src/pages/DataFetch/Tabel.js
+++ src/pages/DataFetch/Tabel.js
@@ -7,7 +7,7 @@ const Tabel = () => {
     useEffect (() => {
         const fetchData = async () => {
             try {
-                const response = await fetch ("https://jsonlaceholder.typicode.com/posts")
+                const response = await fetch ("https://jsonplaceholder.typicode.com/users")
                 const results = await response.json();
                 setdata(results)
             } catch (error) {
