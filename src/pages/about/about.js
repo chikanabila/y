76833b2e6b47f0b70024de27b5c3880a8512f884
chikanabila <119095959+chikanabila@users.noreply.github.com>
2023-12-25 23:28:33 +0700

    Update about.js

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -6,8 +6,12 @@ const about = () => {
     <>
       <Navbar/>
     <h3>Tentang saya</h3>
-    <p>Nama lengkap saya Chika Nabila Orisandi. Saya lahir di Banyuwangi pada tanggal 20 Juni 2004. Saya seorang mahasiswa di STIKOM PGRI Banyuwangi</p>
-
+    <p>Nama lengkap saya Chika Nabila Orisandi. Saya lahir di Banyuwangi pada tanggal 20 Juni 2004. Rumah saya berlamat di Jln. Andalas no.8 Singotrunan, Banyuwangi. Untuk sekarang saya adalah seorang mahasiswa di STIKOM PGRI Banyuwangi</p>
+    <h3>Hubungi saya</h3>
+      <ul>
+        <li>Email: [nchika206@gmail.com]</li>
+        <li>No. telepon: [081334969295]</li>
+    </ul>
     </>
   )
 }
