5fa9e8fa714f053cf5dae414d411aed3248d53e1
chikanabila <119095959+chikanabila@users.noreply.github.com>
2023-12-25 20:35:23 +0700

    Update Input.js

--- src/pages/DataFetch/Input.js
+++ src/pages/DataFetch/Input.js
@@ -6,7 +6,7 @@ const Input = () => {
     useEffect (() => {
         const fetchData = async () => {
             try {
-                const response = await fetch ("https://jsonlaceholder.typicode.com/posts");
+                const response = await fetch ("https://jsonplaceholder.typicode.com/users");
                 const results = await response.json();
                 setData(results);
             } catch (error) {
