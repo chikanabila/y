4c5092230dd19d0008bc97ca41c618a2cd94a859
chikanabila <119095959+chikanabila@users.noreply.github.com>
2023-12-25 20:53:30 +0700

    Update Input.js

--- src/pages/DataFetch/Input.js
+++ src/pages/DataFetch/Input.js
@@ -34,7 +34,7 @@ const Input = () => {
                         <tr key={user.name}>
                             <td>{user.username}</td>
                             <td>{user.email}</td>
-                            <td>{user.address}</td>
+                            <td>{user.address.street}</td>
                             <td>{user.phone}</td>
                         </tr>
                     ))}
